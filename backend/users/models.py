8db56ad6d6152c9534ed1e712ad466d28e78c68c
sirin <pecplantequipment@gmail.com>
2024-07-01 09:27:08 +0700

    Add and migrate users model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.user_name = username
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        master_group = Group.objects.get(name='Master')
+        extra_fields['groups'] = [master_group]
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=50, unique=True)
+    user_password = models.TextField()
+    user_auth = models.CharField(max_length=30)
+    user_phone = models.CharField(max_length=30)
+    user_email = models.EmailField(_('email address'), unique=True)
+    surname = models.CharField(max_length=50, blank=True, null=True)
+    lastname = models.CharField(max_length=50, blank=True, null=True)
+    user_customer = models.CharField(max_length=100, blank=True, null=True)
+    user_address = models.CharField(max_length=50, blank=True, null=True)
+    user_image = models.CharField(max_length=10, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'user_email'
+    REQUIRED_FIELDS = ['user_name', 'user_password']
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text=_('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        verbose_name=_('groups'),
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        verbose_name=_('user permissions'),
+    )
+
+    class Meta:
+        db_table = 'tbl_users_lov'
+        
+    def __str__(self):
+        return self.user_name
