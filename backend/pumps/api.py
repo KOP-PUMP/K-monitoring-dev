36a43c628bbdd54971ed8709d9eb24cceeee350c
sirin <pecplantequipment@gmail.com>
2024-07-05 22:21:09 +0700

    Update permissions

--- backend/pumps/api.py
+++ backend/pumps/api.py
@@ -2,12 +2,13 @@ from ninja_extra import api_controller, http_get, http_post, http_put, http_dele
 from ninja_jwt.authentication import JWTAuth
 from django.shortcuts import get_object_or_404
 from pumps.models import PumpDetail, EngineeringDetail
-from pumps.schemas import PumpDetailIn, PumpDetailOut, EngineeringDetailIn, EngineeringDetailOut
-from pumps.permissions import CanManageEngineering, CanManagePump, CanViewPump
+from pumps.schemas import (PumpDetailIn, PumpDetailOut, EngineeringDetailIn, EngineeringDetailOut)
+
+from pumps.permissions import *
 
 @api_controller('/pumps', tags=['PumpDetail'], auth=JWTAuth())
 class PumpDetailController:
-    @http_post('/', response=PumpDetailOut, permissions=[CanManagePump])
+    @http_post('/', response=PumpDetailOut, permissions=[CanAddPump])
     def create_pump(self, request, payload: PumpDetailIn):
         pump = PumpDetail.objects.create(user=request.user, **payload.dict())
         return pump
@@ -23,7 +24,7 @@ class PumpDetailController:
         pump = get_object_or_404(PumpDetail, pk=pump_id, user=user)
         return pump
 
-    @http_put('/{pump_id}/', response=PumpDetailOut, permissions=[CanManagePump])
+    @http_put('/{pump_id}/', response=PumpDetailOut, permissions=[CanChangePump])
     def update_pump(self, request, pump_id: int, payload: PumpDetailIn):
         user = request.user
         pump = get_object_or_404(PumpDetail, pk=pump_id, user=user)
@@ -32,7 +33,7 @@ class PumpDetailController:
         pump.save()
         return pump
 
-    @http_delete('/{pump_id}/', permissions=[CanManagePump])
+    @http_delete('/{pump_id}/', permissions=[CanDeletePump])
     def delete_pump(self, request, pump_id: int):
         user = request.user
         pump = get_object_or_404(PumpDetail, pk=pump_id, user=user)
@@ -40,23 +41,23 @@ class PumpDetailController:
         return {"success": True}
 
 
-@api_controller('/engineering', tags=['EngineeringDetail'], auth=JWTAuth(), permissions=[CanManageEngineering])
+@api_controller('/engineering', tags=['EngineeringDetail'], auth=JWTAuth(), permissions=[CanViewEngineering, CanAddEngineering, CanChangeEngineering, CanDeleteEngineering])
 class EngineeringDetailController:
-    @http_post('/', response=EngineeringDetailOut)
+    @http_post('/', response=EngineeringDetailOut, permissions=[CanAddEngineering])
     def create_engineering(self, request, payload):
         engineering = EngineeringDetail.objects.create(**payload.dict())
         return engineering
 
-    @http_get('/', response=list[EngineeringDetailOut])
+    @http_get('/', response=list[EngineeringDetailOut], permissions=[CanViewEngineering])
     def list_engineering(self, request):
         return EngineeringDetail.objects.all()
 
-    @http_get('/{pump_id}/', response=EngineeringDetailOut)
+    @http_get('/{pump_id}/', response=EngineeringDetailOut, permissions=[CanViewEngineering])
     def get_engineering(self, request, pump_id: int):
         engineering = get_object_or_404(EngineeringDetail, pk=pump_id)
         return engineering
 
-    @http_put('/{pump_id}/', response=EngineeringDetailOut)
+    @http_put('/{pump_id}/', response=EngineeringDetailOut, permissions=[CanChangeEngineering])
     def update_engineering(self, request, pump_id: int, payload):
         engineering = get_object_or_404(EngineeringDetail, pk=pump_id)
         for attr, value in payload.dict().items():
@@ -64,7 +65,7 @@ class EngineeringDetailController:
         engineering.save()
         return engineering
 
-    @http_delete('/{pump_id}/')
+    @http_delete('/{pump_id}/', permissions=[CanDeleteEngineering])
     def delete_engineering(self, request, pump_id: int):
         engineering = get_object_or_404(EngineeringDetail, pk=pump_id)
         engineering.delete()
--- backend/pumps/permissions.py
+++ backend/pumps/permissions.py
@@ -1,15 +1,34 @@
 from django.http import HttpRequest
 from ninja_extra import ControllerBase, permissions
 
-
-class CanManagePump(permissions.BasePermission):
+class CanAddPump(permissions.BasePermission):
     def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
-        return request.user.has_perm("pumps.manage_pump")
+        return request.user.has_perm("pumps.add_pumpdetail")
+
+class CanChangePump(permissions.BasePermission):
+    def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
+        return request.user.has_perm("pumps.change_pumpdetail")
+
+class CanDeletePump(permissions.BasePermission):
+    def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
+        return request.user.has_perm("pumps.delete_pumpdetail")
 
 class CanViewPump(permissions.BasePermission):
     def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
         return request.user.has_perm("pumps.view_pumpdetail")
 
-class CanManageEngineering(permissions.BasePermission):
+class CanAddEngineering(permissions.BasePermission):
     def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
-        return request.user.has_perm("pumps.manage_engineering")
+        return request.user.has_perm("pumps.add_engineeringdetail")
+
+class CanChangeEngineering(permissions.BasePermission):
+    def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
+        return request.user.has_perm("pumps.change_engineeringdetail")
+
+class CanDeleteEngineering(permissions.BasePermission):
+    def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
+        return request.user.has_perm("pumps.delete_engineeringdetail")
+
+class CanViewEngineering(permissions.BasePermission):
+    def has_permission(self, request: HttpRequest, controller: ControllerBase) -> bool:
+        return request.user.has_perm("pumps.view_engineeringdetail")
