6b65f9a9323251d4701695615d1e8dc917b8f09e
sirin <pecplantequipment@gmail.com>
2024-07-08 15:10:29 +0700

    Modify Schema

---
+++ backend/pumps/schemas/dropdowns.py
@@ -0,0 +1,108 @@
+from ninja import Schema
+
+class ImpellerListSchema(Schema):
+    impeller_type_id: int
+    impeller_type_name: str
+
+class MechSealApiPlanListSchema(Schema):
+    mech_api_id: int
+    mech_api_plan: str
+
+class BearingListSchema(Schema):
+    rotation_de_id: int
+    rotation: str
+
+class CasingMaterialListSchema(Schema):
+    mat_cover_id: int
+    mat_cover_name: str
+    mat_cover_type: str
+
+class FlangRatingListSchema(Schema):
+    flang_rating_id: int
+    flang_rating_name: str
+
+class MechanicalDesignListSchema(Schema):
+    mech_design_id: int
+    mech_design_name: str
+
+class PumpDetailListSchema(Schema):
+    pump_id: int
+    pump_design: str
+    pump_type: str
+
+class MotorDetailListSchema(Schema):
+    motor_drive_id: int
+    drive_system: str | None = None
+    ie_class: str | None = None
+    standard: str | None = None
+
+class SuctionPipeInfoListSchema(Schema):
+    pipe_lov_id: int
+    pipe_sch: str
+    pipe_size: str
+    pipe_id: str
+    fac_number: str
+    equipment: str
+    brand: str
+    short_model: str
+    model: str
+    data_type: str
+    sequence: int
+    rpm: int
+    imp_dia: float
+    flow: float
+    head: float
+    eff: int
+    npshr: float
+    kw: float
+    curve_format: str
+    eff_rl: str
+    eff_status: int
+    eff_distance: float
+    tolerance: int
+    scale_xy: float
+    update_time: str
+    dry_sat: str
+    liquid: float
+
+class PumpStandardListSchema(Schema):
+    pump_standard_id: int
+    name: str
+
+class SuctionDischargeDetailListSchema(Schema):
+    id: int
+    suction_discharge_value: str
+
+class FaceMaterialDetailListSchema(Schema):
+    mat_face_id: int
+    mat_face_name: str
+    mat_face_type: str
+
+class SpringMaterialDetailListSchema(Schema):
+    mat_spring_id: int
+    mat_spring_name: str
+    mat_spring_type: str
+
+class VibrationDetailListSchema(Schema):
+    voltage: str
+    acceptable: str
+    unsatisfied: str
+    unacceptable: str
+
+class DropDownData(Schema):
+    impeller_list: list[ImpellerListSchema]
+    mech_seal_api_plan_list: list[MechSealApiPlanListSchema]
+    bearing_list: list[BearingListSchema]
+    casing_material_list: list[CasingMaterialListSchema]
+    flang_rating_list: list[FlangRatingListSchema]
+    mechanical_design_list: list[MechanicalDesignListSchema]
+    pump_detail_list: list[PumpDetailListSchema]
+    motor_detail_list: list[MotorDetailListSchema]
+    suction_pipe_info_list: list[SuctionPipeInfoListSchema]
+    pump_standard_list: list[PumpStandardListSchema]
+    suction_discharge_detail_list: list[SuctionDischargeDetailListSchema]
+    face_material_detail : list[FaceMaterialDetailListSchema]
+    spring_material_detail : list[SpringMaterialDetailListSchema]
+    vibration_detail : list[VibrationDetailListSchema]
+
+
--- backend/pumps/schemas/units.py
+++ backend/pumps/schemas/units.py
@@ -1,58 +1,93 @@
 from ninja import Schema
 
 class DensityUnit(Schema):
+    id: int
     density_unit: str
 
 class VicosityUnit(Schema):
+    id: int
     viscosity_unit: str
 
 class FlowUnit(Schema):
+    id: int
     flow_unit: str
 
 class PumpSpeedUnit(Schema):
+    id: int
     pump_speed_unit: str
 
 class HeadUnit(Schema):
+    id: int
     head_unit: str
 
 class NpshrUnit(Schema):
+    id: int
     npshr_unit: str
 
 class PowerUnit(Schema):
+    id: int
     power_unit: str
 
 class VoltageUnit(Schema):
+    id: int
     voltage_unit: str
 
 class PumpEfficiencyUnit(Schema):
+    id: int
     pump_efficiency_unit: str
 
 class MechSealSizeUnit(Schema):
+    id: int
     mech_size_unit: str
 
 class PressureGaugeUnit(Schema):
+    id: int
     pres_gauge_unit: str
 
 class PressureUnit(Schema):
+    id: int
     pres_unit: str
 
 class CurrentUnit(Schema):
+    id: int
     current_unit: str
 
 class VibrationUnit(Schema):
+    id: int
     vibration_unit: str
 
 class AccelerationUnit(Schema):
+    id: int
     acceleration_unit: str
 
 class TemperatureUnit(Schema):
+    id: int
     temp_unit: str
 
 class VelocityUnit(Schema):
+    id: int
     velocity_unit: str
 
 class VaporPressureUnit(Schema):
+    id: int
     vapor_pres_unit: str
 
-class UnitsData(DensityUnit, VicosityUnit, FlowUnit, PumpSpeedUnit, HeadUnit, NpshrUnit, PowerUnit, VoltageUnit, PumpEfficiencyUnit, MechSealSizeUnit, PressureGaugeUnit, PressureUnit, CurrentUnit, VibrationUnit, AccelerationUnit, TemperatureUnit, VelocityUnit, VaporPressureUnit):
-    pass    
+class UnitsData(Schema):
+    density_units: list[DensityUnit]
+    viscosity_units: list[VicosityUnit]
+    flow_units: list[FlowUnit]
+    pump_speed_units: list[PumpSpeedUnit]
+    head_units: list[HeadUnit]
+    npshr_units: list[NpshrUnit]
+    power_units: list[PowerUnit]
+    voltage_units: list[VoltageUnit]
+    pump_efficiency_units: list[PumpEfficiencyUnit]
+    mech_seal_size_units: list[MechSealSizeUnit]
+    pressure_gauge_units: list[PressureGaugeUnit]
+    pressure_units: list[PressureUnit]
+    current_units: list[CurrentUnit]
+    vibration_units: list[VibrationUnit]
+    acceleration_units: list[AccelerationUnit]
+    temperature_units: list[TemperatureUnit]
+    velocity_units: list[VelocityUnit]
+    vapor_pressure_units: list[VaporPressureUnit]
