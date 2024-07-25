fe5a5d8591d11ffc4b006773df2b229740942097
sirin <pecplantequipment@gmail.com>
2024-07-25 17:32:20 +0700

    Update authentication api logic

--- frontend/src/api/utils.tsx
+++ frontend/src/api/utils.tsx
@@ -1,21 +1,55 @@
-import { ApiResponse } from "@/types/response";
-import { refreshToken } from "./auth";
+import axios from "axios";
+import AuthService from "@/lib/auth";
 
-export const fetchWithAuth = async <T,>(url: string, options: RequestInit = {}): Promise<ApiResponse<T>> => {
-  // token is in user.access
-  const token = localStorage.getItem("access_token");
-  const response = await fetch(url, {
-    ...options,
-    headers: {
-      ...options.headers,
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
+export const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("access_token"),
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
+);
 
-  const data = await response.json();
-  return data;
-};
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const user = AuthService.getCurrentUser();
+        if (user && user.refresh) {
+          const newTokens = await AuthService.refresh(user.refresh);
+          localStorage.setItem("access_token", newTokens.access);
+          axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${newTokens.access}`;
+          originalRequest.headers["Authorization"] = `Bearer ${newTokens.access}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch (refreshError) {
+        AuthService.logout();
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
