b87024989dd05ffeec0499047f6b0315c99b3372
sirin <pecplantequipment@gmail.com>
2024-07-25 17:35:36 +0700

    Add dropdown api fetch

---
+++ frontend/src/api/dropdown.tsx
@@ -0,0 +1,38 @@
+import { axiosInstance } from "./utils";
+import DropdownSchema from "@/components/types/dropdown";
+import { useQuery } from "@tanstack/react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+
+const fetchDropdownData = async () => {
+  return axiosInstance.get(`${API_BASE_URL}/dropdown`).then((res) => res.data);
+};
+
+const fetchPumpTypes = async () => {
+  return axiosInstance.get(`${API_BASE_URL}/dropdown/types`).then((res) => res.data);
+};
+
+const fetchPumpDesign = async (pump_type: string) => {
+  return axiosInstance.get(`${API_BASE_URL}/dropdown/designs/${pump_type}`).then((res) => res.data);
+};
+
+export const useDropdownData = () => {
+  return useQuery({
+    queryKey: ["dropdown"],
+    queryFn: fetchDropdownData,
+  });
+};
+
+export const useDropdownTypes = () => {
+  return useQuery({
+    queryKey: ["dropdown", "types"],
+    queryFn: fetchPumpTypes,
+  });
+};
+
+export const useDropdownDesigns = (pump_type: string) => {
+  return useQuery({
+    queryKey: ["dropdown", "designs", pump_type],
+    queryFn: () => fetchPumpDesign(pump_type),
+  });
+};
