36f4dbe3c89c7afecf8c5f6ea42b974a6703a8e9
sirin <pecplantequipment@gmail.com>
2024-07-26 09:37:41 +0700

    Create customers.tsx

---
+++ frontend/src/routes/_auth/customers.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { UserTable } from "@/components/UsersTable";
+
+export const Route = createFileRoute("/_auth/customers")({
+  component: UserTable,
+});
