0ca57ff08978aa56be4856d80bce9ce267da81fd
sirin <pecplantequipment@gmail.com>
2024-07-30 06:18:34 +0700

    Fix create_user signal bug

--- backend/users/models.py
+++ backend/users/models.py
@@ -16,7 +16,7 @@ class CustomUserManager(BaseUserManager):
 
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
-        
+
         try:
             user.set_password(password)
             user.save(using=self._db)
--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,20 +1,22 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from users.models import CustomUser, UserProfile
 
+logger = logging.getLogger(__name__)
+
 @receiver(post_save, sender=CustomUser)
 def add_user_to_group(sender, instance, created, **kwargs):
     if created:
         try:
-            customer_group = Group.objects.get_or_create(name='Customer')
-        except:
-            pass
-        if not instance.is_superuser:
-            instance.groups.add(customer_group)
+            customer_group, created = Group.objects.get_or_create(name='Customer')
+            if not instance.is_superuser:
+                instance.groups.add(customer_group)
+        except Exception as e:
+            logger.error(f'Error adding user {instance.username} to group: {e}')
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 @receiver(post_save, sender=CustomUser)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
